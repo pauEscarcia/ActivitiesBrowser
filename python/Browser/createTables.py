9fc10edf6a25145ce206201418af3795c222cbec
pauescarcia <paulina_151090@hotmail.com>
2016-08-08 01:57:20 -0500

    Create table prestador

--- python/Browser/createTables.py
+++ python/Browser/createTables.py
@@ -14,16 +14,16 @@ try:
 	 
 	cursor = db_con.cursor()
 	#cursor.execute("CREATE TABLE clasificacion (idClasificacion BIGSERIAL NOT NULL PRIMARY KEY, nombre TEXT, descripcion TEXT);")
-	cursor.execute("CREATE TABLE categoria (idCategoria BIGSERIAL NOT NULL PRIMARY KEY, nombre TEXT, descripcion TEXT, idClasificacion BIGINT REFERENCES clasificacion(idClasificacion))")
-	 
+	#cursor.execute("CREATE TABLE categoria (idCategoria BIGSERIAL NOT NULL PRIMARY KEY, nombre TEXT, descripcion TEXT, idClasificacion BIGINT REFERENCES clasificacion(idClasificacion))")
+	cursor.execute("CREATE TABLE prestador (idPrestador BIGSERIAL NOT NULL PRIMARY KEY, fecha date, nomOrganizacion TEXT,direccion TEXT, gerente TEXT, email TEXT, pagina TEXT, facebook TEXT, youtube TEXT, whatsapp TEXT, instagram TEXT, twitter TEXT, tumblr TEXT, telEmpresa TEXT, telCel Text, lat TEXT, long TEXT)") 
 	#The variables placeholder must always be a %s, psycop2 will automatically convert the values to SQL literal
 	 
 	#cursor.execute("INSERT INTO clasificacion (nombre,descripcion) VALUES (%s, %s)",("Sports", "lala"))
-	cursor.execute("INSERT INTO categoria (nombre,descripcion,idClasificacion) VALUES (%s, %s, %s)",("hola","uno",1))
-	 
+	#cursor.execute("INSERT INTO categoria (nombre,descripcion,idClasificacion) VALUES (%s, %s, %s)",("hola","uno",1))
+	cursor.execute("INSERT INTO prestador (fecha,nomOrganizacion,direccion,gerente,email,pagina,facebook,youtube,whatsapp,instagram,twitter,tumblr,telEmpresa,telCel,lat,long) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",('2016-07-25','Ally Cat Sailing Adventures','Calle Marina en La Cruz de Huanacaxtle','Juan Perez','allycatsailing@gmail.com','http://allycatsailing.com','facebook','youtube','whatsapp','instagram','twitter','tumblr','3221364230','23432245','207.507.862','-1.053.807.937'))
 	db_con.commit()
 	 
-	cursor.execute("SELECT * FROM categoria")
+	cursor.execute("SELECT * FROM prestador")
 
 	print(cursor.fetchone())
  
